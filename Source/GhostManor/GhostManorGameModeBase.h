f80c0a211427e2d6451d9da87df2b264906bb17f
efentress2025 <efentress@ucsd.edu>
2023-09-19 16:43:05 -0700

    Initial commit

---
+++ Source/GhostManor/GhostManorGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GhostManorGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GHOSTMANOR_API AGhostManorGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
